2afd3acce43632d8e9d76782ba4078571e4a1589
Pascal FREY <frey@macbookpro-de-pascal.home>
2016-02-28 08:23:44 +0100

    minor updates

--- sources/nstokes1_2d.c
+++ sources/nstokes1_2d.c
@@ -725,7 +725,7 @@ int nstokes1_2d(NSst *nsst) {
         fflush(stdout);
       }
       /* save solution */
-      if ( it % nsst->sol.ts == 0 ) {
+      if ( nsst->sol.ts > 0 && it % nsst->sol.ts == 0 ) {
         verb = nsst->info.verb;
         nsst->info.verb = '0';
         saveSol(nsst,jt);
